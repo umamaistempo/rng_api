acaf710a76adf88b42e9ca074576787534119ed8
Charlotte Lorelei de Oliveira <11341349+umamaistempo@users.noreply.github.com>
2020-09-04 12:04:31 -0300

    Test NumberGenerator server

--- lib/rng_api/number_generator.ex
+++ lib/rng_api/number_generator.ex
@@ -1,40 +1,72 @@
 defmodule RngApi.NumberGenerator do
   use GenServer
 
+  require Logger
+
   alias RngApi.Users
   alias RngApi.Users.User
 
-  @spec start_link(any) :: GenServer.on_start()
-  def start_link(_),
-    do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  @type server_option ::
+          {:cronjob, boolean}
+          | {:name, GenServer.name()}
+          | {:setup, (() -> any)}
+
+  @spec start_link([server_option]) :: GenServer.on_start()
+  def start_link(options) do
+    server_name = Keyword.get(options, :name, __MODULE__)
+
+    GenServer.start_link(__MODULE__, options, name: server_name)
+  end
 
   @spec run() :: {DateTime.t() | nil, [User.t()]}
   def run(),
     do: GenServer.call(__MODULE__, :run)
 
   @doc false
-  def init(_) do
-    schedule_recurring_updates()
+  def init(options) do
+    if Keyword.get(options, :cronjob, true) do
+      schedule_recurring_updates()
+    end
 
-    timestamp = nil
-    last_result = {nil, []}
-    {:ok, {max_number(), timestamp, last_result}}
+    # Callback for environment setup, like setting up initial seed for crypto, setting up a repository database etc
+    setup = Keyword.get(options, :setup)
+
+    if is_function(setup, 0) do
+      setup.()
+    end
+
+    # As the `run/0` command returns the _last_ processed result, the first result has to be prepared beforehand;
+    # this block effectively do that by generating the first "result" and progressing the timestamp.
+    initial_timestamp = nil
+    initial_max = max_number()
+    state = result_helper(initial_max, initial_timestamp)
+
+    {:ok, state}
   end
 
   @doc false
   def handle_cast(:update, {_, timestamp, last_result}) do
-    update_users()
+    Logger.info("Updating users point database")
+    {:ok, updated_users} = update_users()
+    Logger.info("Users updated: #{updated_users}")
+
     new_state = {max_number(), timestamp, last_result}
     {:noreply, new_state}
   end
 
   @doc false
   def handle_call(:run, _from, {max_number, timestamp, last_result}) do
-    users = Users.find_users({:points, :>=, max_number}, [:random, limit: 2])
+    new_state = result_helper(max_number, timestamp)
 
-    new_result = {timestamp, users}
+    {:reply, last_result, new_state}
+  end
 
-    {:reply, last_result, {max_number(), now(), new_result}}
+  defp result_helper(initial_max, initial_timestamp) do
+    result = {initial_timestamp, fetch_selected_random_users(initial_max)}
+
+    new_max = max_number()
+    new_timestamp = now()
+    {new_max, new_timestamp, result}
   end
 
   defp max_number(),
@@ -43,6 +75,7 @@ defmodule RngApi.NumberGenerator do
   defp now(),
     do: DateTime.utc_now()
 
+  # Uses erlang's timer to send a message to the RNG server every 60 seconds to get it to execute its update procedure.
   defp schedule_recurring_updates(),
     do: :timer.apply_interval(60_000, GenServer, :cast, [self(), :update])
 
@@ -51,7 +84,18 @@ defmodule RngApi.NumberGenerator do
       User.modify(user, %{points: Enum.random(0..100)})
     end
 
-    # Updates every user on database one-by-one, setting their `points` attribute to a new random value
+    # Updates every user on database one-by-one, setting their `points` attribute to a new random value.
+    # Note that this update is executed through several individual queries which is suboptimal but is the
+    # easiest way to implement this feature while also keeping the business logic on the application side.
+    # Alternatively, one could execute this procedure in up to 3 queries:
+    # 1) Fetch all ids
+    # 2) Execute an update query that maps id -> points
+    # 3) Update all those entries with the same timestamp
+    #
+    # Alternatively the same could be done directly on the database on a single and efficient procedure, but that would take business logic into the database.
     Users.lazy_update({:all, []}, updater)
   end
+
+  defp fetch_selected_random_users(max),
+    do: Users.find_users({:points, :>=, max}, [:random, limit: 2])
 end
---
+++ test/rng_api/number_generator_test.exs
@@ -0,0 +1,124 @@
+defmodule RngApi.NumberGeneratorTest do
+  use ExUnit.Case, async: true
+
+  alias RngApi.NumberGenerator
+  alias RngApi.Repo
+  alias RngApi.Users.User
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    Enum.each(1..100, fn _ -> Repo.insert!(User.create()) end)
+
+    restart_server()
+    :ok
+  end
+
+  test "first call to `run/0` should return nil timestamp" do
+    assert {nil, _} = run()
+  end
+
+  test "subsequent calls to `run/0` should return progressive timestamps" do
+    {nil, _} = run()
+    {t1, _} = run()
+    {t2, _} = run()
+    {t3, _} = run()
+    {t4, _} = run()
+
+    assert :gt == DateTime.compare(t2, t1)
+    assert :gt == DateTime.compare(t3, t2)
+    assert :gt == DateTime.compare(t4, t3)
+  end
+
+  test "`run/0` returns only users whose `points` value is bigger than or equal to the internal max" do
+    Repo.delete_all(User)
+
+    # The number generator should have a `max` between 0 and 100, so by having users with less than 0, they will never be fetched
+    Enum.each(1..100, fn _ ->
+      User.create()
+      |> Ecto.Changeset.put_change(:points, -1)
+      |> Repo.insert!()
+    end)
+
+    restart_server()
+
+    assert {_, []} = run()
+    assert {_, []} = run()
+    assert {_, []} = run()
+    assert {_, []} = run()
+    assert {_, []} = run()
+    assert {_, []} = run()
+    assert {_, []} = run()
+  end
+
+  test "`run/0` returns up to 2 users whose `points` value is bigger than or equal to the internal max" do
+    Repo.delete_all(User)
+
+    # The number generator should have a `max` between 0 and 100, so by having users with less than 0, they will never be fetched
+    Enum.each(1..100, fn _ ->
+      User.create()
+      |> Ecto.Changeset.put_change(:points, -1)
+      |> Repo.insert!()
+    end)
+
+    # Those users have a `points` value bigger than the maximum possible value from the NumberGenerator server, so they
+    # should always be possible selections for the server
+    expected_ids =
+      Enum.map(1..10, fn _ ->
+        User.create()
+        |> Ecto.Changeset.put_change(:points, 999)
+        |> Repo.insert!()
+        |> Map.fetch!(:id)
+      end)
+
+    restart_server()
+
+    for _ <- 1..100 do
+      {_, users} = run()
+
+      assert Enum.count(users) in 0..2
+
+      Enum.each(users, fn user ->
+        assert user.id in expected_ids
+      end)
+    end
+  end
+
+  test "`run/0` returned users are random" do
+    Repo.delete_all(User)
+
+    # Those users have a `points` value bigger than the maximum possible value from the NumberGenerator server, so they
+    # should always be possible selections for the server
+    # Also note that the set of possible users is big enough to make it virtually impossible to have a randomic collision
+    Enum.each(1..100, fn _ ->
+      User.create()
+      |> Ecto.Changeset.put_change(:points, 999)
+      |> Repo.insert!()
+    end)
+
+    restart_server()
+
+    {_, xs1} = run()
+    {_, xs2} = run()
+    {_, xs3} = run()
+
+    assert xs1 != xs2 or xs2 != xs3
+  end
+
+  defp run(),
+    do: GenServer.call(__MODULE__, :run)
+
+  defp restart_server() do
+    test_agent_pid = self()
+
+    sandbox_setup = fn ->
+      Ecto.Adapters.SQL.Sandbox.allow(Repo, test_agent_pid, self())
+    end
+
+    # Stops the local NumberGenerator if it is running, to return it to its initial state
+    if GenServer.whereis(__MODULE__) do
+      GenServer.stop(__MODULE__)
+    end
+
+    NumberGenerator.start_link(name: __MODULE__, cronjob: false, setup: sandbox_setup)
+  end
+end
